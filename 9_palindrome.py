d89ffc6bb87e52ff7c94267dc1e2e212d18081a1
Matu <36888936+Matu94@users.noreply.github.com>
2022-02-02 20:27:00 +0100

    Add files via upload

--- 9_palindrome.py
+++ 9_palindrome.py
@@ -1,18 +1,25 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
+        #x to string
         sx = str(x)
 
-
+        #last index in string (-1 bcs len is start with 1, not 0)
         end = len(sx) - 1
         value = []
 
+
         for i in range(len(sx)):
+            #check if the first and last index is the same then incerement/decrement
+            #if true, add True to the list, else add False
             if sx[i] == sx[end]:
                 value.append(True)
             else:
                 value.append(False)
+
+            #decrement end value ss
             end = end -1
 
+        #check all elements in value, if all the same, result will be true
         result = all(element == True for element in value)
         return(result)
